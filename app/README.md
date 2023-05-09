aee425f4958d1ac50b99642a6c395cc2b2b645c3
diegoperea20 <diegoperea20@gmail.com>
2023-05-09 15:37:02 -0500

    version1

---
+++ app/README.md
@@ -0,0 +1,23 @@
+# Clasification of Reviews about Movies IMBD
+<p align="justify">
+A review sentiment classification model is implemented using different preprocessing and training methods. The IMDB dataset, which consists of movie reviews labeled as positive or negative, is used. First, the dataset is loaded and preprocessed. Then, the model is built, compiled and trained using the training set
+.</p>
+<p align="justify">The application will classify positively or negatively the review that is entered, you can use different models in total 4 that were used to preprocess and train with data from the IMBD dataset.</p>
+<p align="justify">1. Select the model to use</p>
+
+
+![Step1](static/images/step1.PNG)
+
+
+<p align="justify">2. Write a review </p>
+
+![Step2](static/images/step2.PNG)
+
+<p align="justify">3. Click button a "Send" </p>
+
+![Step3](static/images/step3.PNG)
+
+You review is clasificated 
+
+![Step4](static/images/step4.PNG)
+
---
+++ app/app.py
@@ -0,0 +1,64 @@
+from flask import Flask, render_template, request
+from tensorflow.keras.models import load_model
+from tensorflow.keras.datasets import imdb
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST" or request.method == "GET":
+        return render_template("index.html")
+            
+
+@app.route("/clasification", methods=["POST"])
+def clasification():  
+        #--------------------------- 
+        option_selected = request.form['opciones']
+        if option_selected == "opcion4":
+             # Cargar el modelo guardado
+             model = load_model("app\modelo.h5")
+        elif option_selected == "opcion3":
+             model = load_model("app\modelo3.h5")
+        elif option_selected == "opcion2":
+             model = load_model("app\modelo2.h5")
+        else:
+             model = load_model("app\modelou.h5")
+        #--------------------------- 
+       # Cargar el modelo guardado
+       # model = load_model("app\modelo.h5")
+
+        # Prompt the user to enter a review for classification
+        user_review = request.form['review']
+        # the movie was very stupid and bad  the worsdt movie in al times  :negative
+        # nice video : positive
+
+        # Tokenize the user's review using the IMDB dataset's word index
+        word_index = imdb.get_word_index()
+        user_tokens = [
+            word_index[word] if word in word_index else 0
+            for word in user_review.split()
+        ]
+
+        # Pad the user's tokens to a maximum length of 256 words
+        maxlen = 256
+        user_padded = pad_sequences([user_tokens], maxlen=maxlen)
+
+        # Use the trained model to predict the sentiment of the user's review
+        prediction = model.predict(user_padded)[0][0]
+
+        # Print the predicted sentiment
+        if prediction >= 0.5:
+            print(f"Prediction: {prediction}")
+            print("Positive review!")
+            mensaje="Positive review!"
+            return render_template("index.html", prediction=prediction, mensaje=mensaje ,user_review=user_review )
+        else:
+            print(f"Prediction: {prediction}")
+            print("Negative review.")
+            mensaje="Negative review."
+            return render_template("index.html", prediction=prediction, mensaje=mensaje,user_review=user_review )
+
+if __name__ == "__main__":
+    app.run(debug=True)
